112f97a8e715e14de2242e7b7a0180e4d686bb5f
SissyChoi <90140566+SissyChoi@users.noreply.github.com>
2021-11-19 16:24:39 +0100

    Update Opdr_024_Eventhandlers_Script.js

--- Opdr_024_Eventhandlers_Script.js
+++ Opdr_024_Eventhandlers_Script.js
@@ -8,10 +8,11 @@ buttonAlert.addEventListener("click", function(buttonClicked) {
 //Part 2
 const buttonBlue = document.getElementById("mybutton-blue-background")
 
+/*Deze code is niet meer nodig voor Part 3
 buttonBlue.addEventListener("click", function(makeButtonRed) {
     buttonBlue.style.backgroundColor = "red";
     buttonBlue.className.add("red-background")
-});
+}); */
 
 //Part 3
 const buttonToggle = function (buttonToggle) {
